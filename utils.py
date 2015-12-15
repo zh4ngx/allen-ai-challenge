efeec5571df8c5499a6e43d6b4e6b18bfaf140dd
Andy Zhang <signupforandy@gmail.com>
2015-12-14 20:42:59 -0800

    Move preprocessing and scoring into util

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
+import string
+
 # Convert index in set(0, 1, 2, 3) to answer in set('A', 'B', 'C', 'D')
-def idx2answerchar(idx):
+def idx2answerlabel(idx):
     return chr(idx + 65)
 
+# Preprocess text for querying gensim model
+def preprocess_text_gensim(model, text):
+    words = text.lower().translate(None, string.punctuation).split()
+    return [word for word in words if model.__contains__(word)]
+
+# Choose answer based on highest score
+def choose_answer(score_dict):
+    return max(score_dict.iteritems(), key=lambda item: item[1])[0]
